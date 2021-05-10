56d506952e0d3f65860a84ee9c1a99a9aece2cc2
pratham31012002 <pratham31012002@gmail.com>
2021-05-10 20:53:55 +0530

    feat: added functionality

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const db = window.firebase.firestore()
+db.settings({ timestampsInSnapshots: true })
 const mon = document.querySelectorAll('.mon')[0]
 const tue = document.querySelectorAll('.tue')[0]
 const wed = document.querySelectorAll('.wed')[0]
@@ -6,6 +8,64 @@ const fri = document.querySelectorAll('.fri')[0]
 const sat = document.querySelectorAll('.sat')[0]
 const sun = document.querySelectorAll('.sun')[0]
 const main = document.querySelectorAll('.main')[0]
+const upload = document.querySelectorAll('.upload1')[0]
+
+window.onload = function () {
+  const monday = JSON.parse(window.localStorage.getItem('monday'))
+  const tuesday = JSON.parse(window.localStorage.getItem('tuesday'))
+  const wednesday = JSON.parse(window.localStorage.getItem('wednesday'))
+  const thursday = JSON.parse(window.localStorage.getItem('thursday'))
+  const friday = JSON.parse(window.localStorage.getItem('friday'))
+  window.console.log(monday)
+  window.console.log(tuesday)
+  window.console.log(wednesday)
+  window.console.log(thursday)
+  window.console.log(friday)
+  let text = ''
+  for (let i = 0; i < monday.length; i++) {
+    text = text +
+  `
+  <div class="main1">
+  <div class="details">
+      <div class="name">${monday[i].Course_Name}</div>
+      <div class="time">8:00 AM - 8:40 AM</div>
+  </div>
+  <div class="edit">Edit</div>
+  <div class="arrow"><img src="../images/Vector.svg" alt="dropdown arrow" class="src"></div>
+</div>
+  `
+  }
+  main.innerHTML = text
+  for (let j = 0; j < monday.length; j++) {
+    document.querySelectorAll('.edit')[j].addEventListener('click', () => {
+      const det = window.prompt('Enter details')
+      console.log(det)
+      const myHeaders = new Headers()
+      myHeaders.append('Accept', 'application/json')
+      myHeaders.append('Content-Type', 'application/x-www-form-urlencoded')
+
+      const urlencoded = new URLSearchParams()
+      urlencoded.append('request', det)
+
+      const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: 'follow'
+      }
+
+      fetch('http://13.233.74.155/uploadtext/', requestOptions)
+        .then(response => response.json())
+        .then(result => {
+          document.querySelectorAll('.name')[j].innerHTML = result.Slots[0].Course_Name
+          monday[j] = result.Slots[0]
+          console.log(monday)
+          window.localStorage.setItem('monday', JSON.stringify(monday))
+        })
+        .catch(error => console.log('error', error))
+    })
+  }
+}
 
 function remove () {
   mon.removeAttribute('id')
@@ -20,96 +80,247 @@ function remove () {
 mon.addEventListener('click', function () {
   remove()
   mon.id = 'special'
-  main.innerHTML =
+  const monday = JSON.parse(window.localStorage.getItem('monday'))
+  let text = ''
+  for (let i = 0; i < monday.length; i++) {
+    text = text +
   `
   <div class="main1">
   <div class="details">
-      <div class="name">Java Programming</div>
+      <div class="name">${monday[i].Course_Name}</div>
       <div class="time">8:00 AM - 8:40 AM</div>
   </div>
-  <div class="arrow"><img src="../images/Vector.svg" alt="dropdown arrow" class="src"></div>
-</div>
-<div class="main1" id="special1">
-  <div class="details">
-      <div class="name">Python Programming</div>
-      <div class="time">8:00 AM - 8:40 AM</div>
-  </div>
-  <div class="arrow"><img src="../images/Vector.svg" alt="dropdown arrow" class="src"></div>
-</div>
-<div class="main1">
-  <div class="details">
-      <div class="name">C Programming</div>
-      <div class="time">8:00 AM - 8:40 AM</div>
-  </div>
+  <div class="edit">Edit</div>
   <div class="arrow"><img src="../images/Vector.svg" alt="dropdown arrow" class="src"></div>
 </div>
   `
+  }
+  main.innerHTML = text
+  for (let j = 0; j < monday.length; j++) {
+    document.querySelectorAll('.edit')[j].addEventListener('click', () => {
+      const det = window.prompt('Enter details')
+      console.log(det)
+      const myHeaders = new Headers()
+      myHeaders.append('Accept', 'application/json')
+      myHeaders.append('Content-Type', 'application/x-www-form-urlencoded')
+
+      const urlencoded = new URLSearchParams()
+      urlencoded.append('request', det)
+
+      const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: 'follow'
+      }
+
+      fetch('http://13.233.74.155/uploadtext/', requestOptions)
+        .then(response => response.json())
+        .then(result => {
+          document.querySelectorAll('.name')[j].innerHTML = result.Slots[0].Course_Name
+          monday[j] = result.Slots[0]
+          console.log(monday)
+          window.localStorage.setItem('monday', JSON.stringify(monday))
+        })
+        .catch(error => console.log('error', error))
+    })
+  }
 })
 tue.addEventListener('click', function () {
   remove()
   tue.id = 'special'
-  main.innerHTML =
+  const tuesday = JSON.parse(window.localStorage.getItem('tuesday'))
+  let text = ''
+  for (let i = 0; i < tuesday.length; i++) {
+    text = text +
   `
   <div class="main1">
   <div class="details">
-      <div class="name">Java</div>
+      <div class="name">${tuesday[i].Course_Name}</div>
       <div class="time">8:00 AM - 8:40 AM</div>
   </div>
-  <div class="arrow"><img src="../images/Vector.svg" alt="dropdown arrow" class="src"></div>
-</div>
-<div class="main1" id="special1">
-  <div class="details">
-      <div class="name">Python</div>
-      <div class="time">8:00 AM - 8:40 AM</div>
-  </div>
-  <div class="arrow"><img src="../images/Vector.svg" alt="dropdown arrow" class="src"></div>
-</div>
-<div class="main1">
-  <div class="details">
-      <div class="name">C</div>
-      <div class="time">8:00 AM - 8:40 AM</div>
-  </div>
+  <div class="edit">Edit</div>
   <div class="arrow"><img src="../images/Vector.svg" alt="dropdown arrow" class="src"></div>
 </div>
   `
+  }
+  main.innerHTML = text
+  for (let j = 0; j < tuesday.length; j++) {
+    document.querySelectorAll('.edit')[j].addEventListener('click', () => {
+      const det = window.prompt('Enter details')
+      console.log(det)
+      const myHeaders = new Headers()
+      myHeaders.append('Accept', 'application/json')
+      myHeaders.append('Content-Type', 'application/x-www-form-urlencoded')
+
+      const urlencoded = new URLSearchParams()
+      urlencoded.append('request', det)
+
+      const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: 'follow'
+      }
+
+      fetch('http://13.233.74.155/uploadtext/', requestOptions)
+        .then(response => response.json())
+        .then(result => {
+          document.querySelectorAll('.name')[j].innerHTML = result.Slots[0].Course_Name
+          tuesday[j] = result.Slots[0]
+          console.log(tuesday)
+          window.localStorage.setItem('tuesday', JSON.stringify(tuesday))
+        })
+        .catch(error => console.log('error', error))
+    })
+  }
 })
 wed.addEventListener('click', function () {
   remove()
   wed.id = 'special'
-  main.innerHTML =
+  const wednesday = JSON.parse(window.localStorage.getItem('wednesday'))
+  let text = ''
+  for (let i = 0; i < wednesday.length; i++) {
+    text = text +
   `
   <div class="main1">
   <div class="details">
-      <div class="name">hello</div>
+      <div class="name">${wednesday[i].Course_Name}</div>
       <div class="time">8:00 AM - 8:40 AM</div>
   </div>
-  <div class="arrow"><img src="../images/Vector.svg" alt="dropdown arrow" class="src"></div>
-</div>
-<div class="main1" id="special1">
-  <div class="details">
-      <div class="name">world</div>
-      <div class="time">8:00 AM - 8:40 AM</div>
-  </div>
-  <div class="arrow"><img src="../images/Vector.svg" alt="dropdown arrow" class="src"></div>
-</div>
-<div class="main1">
-  <div class="details">
-      <div class="name">how</div>
-      <div class="time">8:00 AM - 8:40 AM</div>
-  </div>
+  <div class="edit">Edit</div>
   <div class="arrow"><img src="../images/Vector.svg" alt="dropdown arrow" class="src"></div>
 </div>
   `
+  }
+  main.innerHTML = text
+  for (let j = 0; j < wednesday.length; j++) {
+    document.querySelectorAll('.edit')[j].addEventListener('click', () => {
+      const det = window.prompt('Enter details')
+      console.log(det)
+      const myHeaders = new Headers()
+      myHeaders.append('Accept', 'application/json')
+      myHeaders.append('Content-Type', 'application/x-www-form-urlencoded')
+
+      const urlencoded = new URLSearchParams()
+      urlencoded.append('request', det)
+
+      const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: 'follow'
+      }
+
+      fetch('http://13.233.74.155/uploadtext/', requestOptions)
+        .then(response => response.json())
+        .then(result => {
+          document.querySelectorAll('.name')[j].innerHTML = result.Slots[0].Course_Name
+          wednesday[j] = result.Slots[0]
+          console.log(wednesday)
+          window.localStorage.setItem('wednesday', JSON.stringify(wednesday))
+        })
+        .catch(error => console.log('error', error))
+    })
+  }
 })
 thu.addEventListener('click', function () {
   remove()
   thu.id = 'special'
-  main.innerHTML = ''
+  const thursday = JSON.parse(window.localStorage.getItem('thursday'))
+  let text = ''
+  for (let i = 0; i < thursday.length; i++) {
+    text = text +
+  `
+  <div class="main1">
+  <div class="details">
+      <div class="name">${thursday[i].Course_Name}</div>
+      <div class="time">8:00 AM - 8:40 AM</div>
+  </div>
+  <div class="edit">Edit</div>
+  <div class="arrow"><img src="../images/Vector.svg" alt="dropdown arrow" class="src"></div>
+</div>
+  `
+  }
+  main.innerHTML = text
+  for (let j = 0; j < thursday.length; j++) {
+    document.querySelectorAll('.edit')[j].addEventListener('click', () => {
+      const det = window.prompt('Enter details')
+      console.log(det)
+      const myHeaders = new Headers()
+      myHeaders.append('Accept', 'application/json')
+      myHeaders.append('Content-Type', 'application/x-www-form-urlencoded')
+
+      const urlencoded = new URLSearchParams()
+      urlencoded.append('request', det)
+
+      const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: 'follow'
+      }
+
+      fetch('http://13.233.74.155/uploadtext/', requestOptions)
+        .then(response => response.json())
+        .then(result => {
+          document.querySelectorAll('.name')[j].innerHTML = result.Slots[0].Course_Name
+          thursday[j] = result.Slots[0]
+          console.log(thursday)
+          window.localStorage.setItem('thursday', JSON.stringify(thursday))
+        })
+        .catch(error => console.log('error', error))
+    })
+  }
 })
 fri.addEventListener('click', function () {
   remove()
   fri.id = 'special'
-  main.innerHTML = ''
+  const friday = JSON.parse(window.localStorage.getItem('friday'))
+  let text = ''
+  for (let i = 0; i < friday.length; i++) {
+    text = text +
+  `
+  <div class="main1">
+  <div class="details">
+      <div class="name">${friday[i].Course_Name}</div>
+      <div class="time">8:00 AM - 8:40 AM</div>
+  </div>
+  <div class="edit">Edit</div>
+  <div class="arrow"><img src="../images/Vector.svg" alt="dropdown arrow" class="src"></div>
+</div>
+  `
+  }
+  main.innerHTML = text
+  for (let j = 0; j < friday.length; j++) {
+    document.querySelectorAll('.edit')[j].addEventListener('click', () => {
+      const det = window.prompt('Enter details')
+      console.log(det)
+      const myHeaders = new Headers()
+      myHeaders.append('Accept', 'application/json')
+      myHeaders.append('Content-Type', 'application/x-www-form-urlencoded')
+
+      const urlencoded = new URLSearchParams()
+      urlencoded.append('request', det)
+
+      const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: 'follow'
+      }
+
+      fetch('http://13.233.74.155/uploadtext/', requestOptions)
+        .then(response => response.json())
+        .then(result => {
+          document.querySelectorAll('.name')[j].innerHTML = result.Slots[0].Course_Name
+          friday[j] = result.Slots[0]
+          console.log(friday)
+          window.localStorage.setItem('friday', JSON.stringify(friday))
+        })
+        .catch(error => console.log('error', error))
+    })
+  }
 })
 sat.addEventListener('click', function () {
   remove()
@@ -120,4 +331,67 @@ sun.addEventListener('click', function () {
   remove()
   sun.id = 'special'
   main.innerHTML = ''
+})
+
+upload.addEventListener('click', function () {
+  const monday = JSON.parse(window.localStorage.getItem('monday'))
+  const tuesday = JSON.parse(window.localStorage.getItem('tuesday'))
+  const wednesday = JSON.parse(window.localStorage.getItem('wednesday'))
+  const thursday = JSON.parse(window.localStorage.getItem('thursday'))
+  const friday = JSON.parse(window.localStorage.getItem('friday'))
+  for (let i = 0; i < monday.length; i++) {
+    db.collection('users').doc('bSV6dmzNndR8VTo2hPHHuDZBZeD2').collection('timetable').doc('monday').collection('periods').add({
+      slot: monday[i].Slot,
+      courseCode: monday[i].Course_Name,
+      courseName: monday[i].Course_Name,
+      Course_type: monday[i].Course_type,
+      location: monday[i].Venue,
+      endTime: '8 May 2021 at 22:45:00 UTC+5:30',
+      startTime: '8 May 2021 at 22:45:00 UTC+5:30'
+    })
+  }
+  for (let i = 0; i < tuesday.length; i++) {
+    db.collection('users').doc('bSV6dmzNndR8VTo2hPHHuDZBZeD2').collection('timetable').doc('tuesday').collection('periods').add({
+      slot: tuesday[i].Slot,
+      courseCode: tuesday[i].Course_Name,
+      courseName: tuesday[i].Course_Name,
+      Course_type: tuesday[i].Course_type,
+      location: tuesday[i].Venue,
+      endTime: '8 May 2021 at 22:45:00 UTC+5:30',
+      startTime: '8 May 2021 at 22:45:00 UTC+5:30'
+    })
+  }
+  for (let i = 0; i < wednesday.length; i++) {
+    db.collection('users').doc('bSV6dmzNndR8VTo2hPHHuDZBZeD2').collection('timetable').doc('wednesday').collection('periods').add({
+      slot: wednesday[i].Slot,
+      courseCode: wednesday[i].Course_Name,
+      courseName: wednesday[i].Course_Name,
+      Course_type: wednesday[i].Course_type,
+      location: wednesday[i].Venue,
+      endTime: '8 May 2021 at 22:45:00 UTC+5:30',
+      startTime: '8 May 2021 at 22:45:00 UTC+5:30'
+    })
+  }
+  for (let i = 0; i < thursday.length; i++) {
+    db.collection('users').doc('bSV6dmzNndR8VTo2hPHHuDZBZeD2').collection('timetable').doc('thursday').collection('periods').add({
+      slot: thursday[i].Slot,
+      courseCode: thursday[i].Course_Name,
+      courseName: thursday[i].Course_Name,
+      Course_type: thursday[i].Course_type,
+      location: thursday[i].Venue,
+      endTime: '8 May 2021 at 22:45:00 UTC+5:30',
+      startTime: '8 May 2021 at 22:45:00 UTC+5:30'
+    })
+  }
+  for (let i = 0; i < friday.length; i++) {
+    db.collection('users').doc('bSV6dmzNndR8VTo2hPHHuDZBZeD2').collection('timetable').doc('friday').collection('periods').add({
+      slot: friday[i].Slot,
+      courseCode: friday[i].Course_Name,
+      courseName: friday[i].Course_Name,
+      Course_type: friday[i].Course_type,
+      location: friday[i].Venue,
+      endTime: '8 May 2021 at 22:45:00 UTC+5:30',
+      startTime: '8 May 2021 at 22:45:00 UTC+5:30'
+    })
+  }
 })
---
+++ src/auth.js
@@ -0,0 +1,32 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+document.getElementsByClassName('login')[0].addEventListener('click', () => {
+  console.log('Yes')
+  auth()
+})
+
+function auth () {
+  const provider = new firebase.auth.GoogleAuthProvider()
+  firebase.auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+      const credential = result.credential
+
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const token = credential.accessToken
+      // The signed-in user info.
+      const user = result.user
+    // ...
+    }).catch((error) => {
+    // Handle Errors here.
+      const errorCode = error.code
+      const errorMessage = error.message
+      // The email of the user's account used.
+      const email = error.email
+      // The firebase.auth.AuthCredential type that was used.
+      const credential = error.credential
+    // ...
+    })
+}
