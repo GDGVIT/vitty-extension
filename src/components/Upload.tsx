c0830c2f1077bd1414fb483fbe7ed8a837eacaa1
Krish Chatterjie <chatterjie.krish@gmail.com>
2022-01-21 11:43:56 +0530

    feat: not uploaded page

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import './../styles/LoggedIn.css'
 
 const Upload: React.FC = (): JSX.Element => {
+  const handleClick = (): void => {
+    const newURL = 'https://vitty.dscvit.com'
+    void chrome.tabs.create({ url: newURL, active: true })
+  }
   return (
     <div className='upload-wrapper'>
-      <h1>Upload Timetable</h1>
+      <h1>Timetable Missing!</h1>
       <div className='upload'>
-        Please upload on vitty.dscvit.com
+        <h2>Please upload your timetable on <span onClick={handleClick}>vitty.dscvit.com</span></h2>
       </div>
     </div>
   )
